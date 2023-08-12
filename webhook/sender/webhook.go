59460cbb4c5904738ab390e343964159534d8107
koladev <onaelmangabo@gmail.com>
2023-08-13 00:13:19 +0100

    ⚰️ remove db usage to simplify

--- webhook/sender/webhook.go
+++ webhook/sender/webhook.go
@@ -2,13 +2,10 @@ package sender
 
 import (
 	"bytes"
-	"database/sql"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"os"
 )
 
 // Payload represents the structure of the data expected to be sent as a webhook
@@ -26,13 +23,6 @@ func SendWebhook(data interface{}, url string, webhookId string) error {
 	if err != nil {
 		return err
 	}
-
-	// Retrieve database connection information from environment variables
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
 
 	// Prepare the webhook request
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBytes))
@@ -59,32 +49,7 @@ func SendWebhook(data interface{}, url string, webhookId string) error {
 		status = "delivered"
 	}
 
-	// Update the status in the database
-	if err := updateStatus(webhookId, status, dbUser, dbPassword, dbHost, dbPort, dbName); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// updateStatus connects to the database and updates the status of the event with the given webhookId
-func updateStatus(webhookId, status, dbUser, dbPassword, dbHost, dbPort, dbName string) error {
-	// Connect to the PostgreSQL database
-	db, err := sql.Open("postgres", fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName))
-	if err != nil {
-		return err
-	}
-	defer func(db *sql.DB) {
-		if err := db.Close(); err != nil {
-			log.Println("Error closing database connection:", err)
-		}
-	}(db)
-
-	// Update the event status
-	_, err = db.Exec("UPDATE events SET status=$1 WHERE id=$2", status, webhookId)
-	if err != nil {
-		return err
-	}
+	log.Println(status)
 
 	return nil
 }
