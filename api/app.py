f651958a268ca3868d9c73e72b8f387c49c79bd7
koladev <onaelmangabo@gmail.com>
2023-08-12 22:54:36 +0100

    ✨  add docker file

--- api/app.py
+++ api/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/payment')
+def payment():
+    return 'Processing'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
